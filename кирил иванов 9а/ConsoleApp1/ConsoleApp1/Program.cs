05cd9015c75daa6b1be6d4e73b6631cfab7f22ea
KirilIvano <kiro.ivanov.0209@gmail.com>
2024-12-09 13:21:42 +0200

    Add files via upload

---
+++ кирил иванов 9а/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String ime;
+            //Console.WriteLine("Kak se kazva6?");
+            //Console.ReadLine();
+            Console.Beep(432, 500);
+            for (int i = 200; i <= 4000; i += 200)
+            {
+                Console.Beep(i, 100);
+            }
+                Console.Write("Hello,");
+                Console.WriteLine("Kiro");
+                Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+
+        }
+    }
+}
+
+
+
